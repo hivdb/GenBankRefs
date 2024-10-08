e099206db88848cdc2c5da649d41b32fcec8e52d
rshaferw <rshafer@stanford.edu>
2024-10-08 10:32:04 -0700

    Removed unused code; created convert_dict_to_list_of_sets
    Also required the year to be the same in compare_author_titles. This led to a small reduction in combined references. However, review of Merged_Author_titles_edited.xlsx reveals many more references that could be merged.

--- GenBankFunctions.py
+++ GenBankFunctions.py
@@ -2,7 +2,6 @@ from Bio import SeqIO
 from Bio import Entrez
 import pandas as pd
 import re
-import Levenshtein
 
 
 Entrez.email = "rshafer.stanford.edu"
@@ -24,89 +23,11 @@ def is_reference_genome(acc):
     return False
 
 
-def string_edit_dist(string1, string2):
-    distance = Levenshtein.distance(string1, string2)
-    #print("Levenshtein Distance:", distance)
-    return distance
-
 def process_author_field(text):
     text = text.replace(' and', ',')
     authors = text.split('.,')
     return tuple(authors)
 
-# This can be converted into 1 or 2 lines
-def compare_accession_lists(list1, list2):
-    set1 = set(list1)
-    set2 = set(list2)
-    if set1 == set2:
-        return 1
-    else:
-        return 0
-
-# This is currently not being used because we are requiring the author lists
-# to be identical to merge references
-def compare_author_lists(list1, list2):
-    set1 = set(list1)
-    set2 = set(list2)
-    if set1.issubset(set2) or set2.issubset(set1):
-        return 1
-    else:
-        shared_set = set1 & set2
-        pcnt_set1 = len(shared_set) / len(set1)
-        pcnt_set2 = len(shared_set) / len(set1)
-        max_pcnt = max(pcnt_set1, pcnt_set2)
-        return max_pcnt
-
-
-def process_accession_lists(df, logfile):
-    file = open(logfile, 'w')
-    accession_list = df['accession']
-    close_lists = {}
-    for i, item1 in enumerate(accession_list):
-        close_matches = []
-        for j, item2 in enumerate(accession_list):
-            if i >= j:
-                continue
-            score = compare_accession_lists(item1, item2)
-            if score == 1:
-                if is_reference_genome(item1[0]):
-                    continue
-                close_matches.append(j)
-                file.write(f'{i}: {item1}\n{j}:{item2}\n\n')
-        if len(close_matches) >=1:
-            close_lists[i] = close_matches
-
-    list_of_shared_indexes = []
-    for index, value in close_lists.items():
-        shared_indexes = set()
-        shared_indexes.add(index)
-        shared_indexes.update(set(value))
-        subset_flag = False
-        for items in list_of_shared_indexes:
-            if shared_indexes.issubset(items):
-                subset_flag = True
-        if subset_flag == True:
-            continue
-        list_of_shared_indexes.append(shared_indexes)
-    print(f'''number with same accessions: {len(list_of_shared_indexes)}:   
-          {list_of_shared_indexes}''')
-
-    indexes_of_rows_to_be_dropped = [
-        j
-        for i in list_of_shared_indexes
-        for j in i
-    ]
-    print('Dropped', len(indexes_of_rows_to_be_dropped))
-    list_of_new_rows = []
-    for item in list_of_shared_indexes:
-        new_row = merge_refs_sharing_accessions(df, list(item))
-        list_of_new_rows.append(new_row)
-
-    df = df.drop(indexes_of_rows_to_be_dropped)
-    list_of_new_rows.insert(0, df)
-    list_of_new_rows = pd.concat(list_of_new_rows, ignore_index=True)
-
-    return list_of_new_rows
 
 # The score is 1 if the author list is the same and 1 of the following criteria is met
 # (1) One or both of the titles are 'Direct Submission" and the years are within 1. If the years
@@ -135,6 +56,64 @@ def compare_authors_titles(row_i, row_j):
     return score
 
 
+#Convert {key: [values], key: [values]] to a list of sets comprising {key, values}
+#In this program, each key is the index of a row and the values contain
+# one or more indexes of rows that share some property
+#The subset flag code ensures sets do not share items with one another
+def convert_dict_to_list_of_sets(dict):
+    list_of_sets = []
+    list_of_items = []
+    for key, values in dict.items():
+        key_plus_values = set()
+        key_plus_values.add(key)
+        key_plus_values.update(set(values))    
+        subset_flag = False
+        for items in list_of_sets:
+            if key_plus_values.issubset(items):
+                subset_flag = True
+        if subset_flag == True:
+            continue
+        list_of_sets.append(key_plus_values)
+    
+    list_of_items = [
+        j
+        for i in list_of_sets
+        for j in i
+    ]
+
+    return (list_of_sets, list_of_items)
+
+
+def process_accession_lists(df, logfile):
+    file = open(logfile, 'w')
+    accession_list = df['accession']
+    close_lists = {}
+    for i, item1 in enumerate(accession_list):
+        close_matches = []
+        for j, item2 in enumerate(accession_list):
+            if i >= j:
+                continue
+            if set(item1) == set(item2) and is_reference_genome(item1[0]) == False:
+                close_matches.append(j)
+                file.write(f'{i}: {item1}\n{j}:{item2}\n\n')
+        if len(close_matches) >=1:
+            close_lists[i] = close_matches
+
+    list_of_sets_w_shared_indexes, complete_list_of_shared_indexes = convert_dict_to_list_of_sets(close_lists)  
+    print(f'''No with shared accessions: {len(list_of_sets_w_shared_indexes)}: {list_of_sets_w_shared_indexes}''')
+    print(f'''To be dropped: {len(complete_list_of_shared_indexes)}: {complete_list_of_shared_indexes}''')
+    
+    list_of_new_rows = []
+    for item in list_of_sets_w_shared_indexes:
+        new_row = merge_refs_sharing_accessions(df, list(item))
+        list_of_new_rows.append(new_row)
+
+    df = df.drop(complete_list_of_shared_indexes)
+    list_of_new_rows.insert(0, df)
+    list_of_new_rows = pd.concat(list_of_new_rows, ignore_index=True)
+    return list_of_new_rows
+
+
 def process_authors_titles(df, logfile):
     file = open(logfile, 'w')
     close_lists = {} 
@@ -149,53 +128,34 @@ def process_authors_titles(df, logfile):
                 file.write(f'{i}: {row_i}\n {j}: {row_j}\n\n')
             if len(close_matches) >=1:
                 close_lists[i] = close_matches
+    print("Close lists:", close_lists)
 
-    list_of_shared_indexes = []
-    for index, value in close_lists.items():
-        shared_indexes = set()
-        shared_indexes.add(index)
-        shared_indexes.update(set(value))
-        subset_flag = False
-        for items in list_of_shared_indexes:
-            if shared_indexes.issubset(items):
-                subset_flag = True
-        if subset_flag == True:
-            continue
-        list_of_shared_indexes.append(shared_indexes)
-    
-    print(f'''number with shared author title: {len(list_of_shared_indexes)}:   
-          {list_of_shared_indexes}''')        
-    indexes_of_rows_to_be_dropped = [
-        j
-        for i in list_of_shared_indexes
-        for j in i
-    ]
+    list_of_sets_w_shared_indexes, complete_list_of_shared_indexes = convert_dict_to_list_of_sets(close_lists)  
+    print(f'''No with shared accessions: {len(list_of_sets_w_shared_indexes)}: {list_of_sets_w_shared_indexes}''')
+    print(f'''To be dropped: {len(complete_list_of_shared_indexes)}: {complete_list_of_shared_indexes}''')      
 
-    print('To be dropped', len(indexes_of_rows_to_be_dropped))
     list_of_new_rows = []
-    for item in list_of_shared_indexes:
+    for item in list_of_sets_w_shared_indexes:
         new_row = merge_refs_sharing_author_title(df, list(item))
         list_of_new_rows.append(new_row)
 
-    df = df.drop(indexes_of_rows_to_be_dropped)
+    df = df.drop(complete_list_of_shared_indexes)
     list_of_new_rows.insert(0, df)
     list_of_new_rows = pd.concat(list_of_new_rows, ignore_index=True)
     return list_of_new_rows
 
 
 def merge_refs_sharing_accessions(df, shared_indexes):
-    #print(df)
     new_row = {}
-    author_list_list = df.loc[shared_indexes, 'author list'].tolist()
-    authors_list = df.loc[shared_indexes, 'authors'].tolist()
+    author_list = df.loc[shared_indexes, 'author list'].tolist()
+    authors = df.loc[shared_indexes, 'authors'].tolist()
     titles_list = df.loc[shared_indexes, 'title'].tolist()
     journal_list = df.loc[shared_indexes, 'journal'].tolist()
     pmid_list = df.loc[shared_indexes, 'pmid'].tolist()
     year_list = df.loc[shared_indexes, 'year'].tolist()
-    print(year_list)
     accession_list = df.loc[shared_indexes, 'accession'].tolist()
-    new_row['author list'] = [max(author_list_list, key=len)]
-    new_row['authors'] = [max(authors_list, key=len)]
+    new_row['author list'] = [max(author_list, key=len)]
+    new_row['authors'] = [max(authors, key=len)]
     new_row['year'] = [','.join(str(i) for i in year_list if i)]
     new_row['title'] = [max(titles_list, key=len)]
     new_row['pmid'] = ['; '.join(i for i in pmid_list if i)]
@@ -206,15 +166,15 @@ def merge_refs_sharing_accessions(df, shared_indexes):
 
 def merge_refs_sharing_author_title(df, shared_indexes):
     new_row = {}
-    author_list_list = df.loc[shared_indexes, 'author list'].tolist()
-    authors_list = df.loc[shared_indexes, 'authors'].tolist()
+    author_list = df.loc[shared_indexes, 'author list'].tolist()
+    authors = df.loc[shared_indexes, 'authors'].tolist()
     titles_list = df.loc[shared_indexes, 'title'].tolist()
     journal_list = df.loc[shared_indexes, 'journal'].tolist()
     pmid_list = df.loc[shared_indexes, 'pmid'].tolist()
     year_list = df.loc[shared_indexes, 'year'].tolist()
     accession_list = df.loc[shared_indexes, 'accession'].tolist()
-    new_row['author list'] = author_list_list[:1] 
-    new_row['authors'] = authors_list[:1]
+    new_row['author list'] = author_list[:1] 
+    new_row['authors'] = authors[:1]
     new_row['year'] = [';'.join(str(i) for i in year_list if i)]
     new_row['title'] = ['\n'.join(i for i in titles_list)]
     new_row['pmid'] = ['; '.join(i for i in pmid_list if i)]
@@ -227,31 +187,4 @@ def merge_refs_sharing_author_title(df, shared_indexes):
     new_row['accession'] = ','.join(combined_list_accessions)
     new_row_df = pd.DataFrame(new_row)
     return new_row_df
-
-# def fetch_popset_info(uid):
-#     # Query PopSet database with the UID
-#     handle = Entrez.efetch(db="popset", id=uid, rettype="acc", retmode="text")
-#     record = Entrez.read(handle)
-#     print(record)
-#     acc_ids = [id.strip() for id in handle]
-#     handle.close()
-
-# def search_popsets_for_virus(virus_name):
-#     # Perform a search in the PopSet database using the virus name
-#     search_query = f"{virus_name}[Organism]"
-#     handle = Entrez.esearch(db="popset", term=search_query, retmax=200)
-#     record = Entrez.read(handle)
-#     handle.close()
-
-#     # Get the list of UIDs (PopSet IDs) from the search results
-#     uid_list = record["IdList"]
-#     print(uid_list)
-
-#     # If there are results, fetch detailed info about each PopSet
-#     if uid_list:
-#         print(f"Found {len(uid_list)} PopSets for {virus_name}.")
-#         for uid in uid_list:
-#             fetch_popset_info(uid)  # Function from previous example
-#     else:
-#         print(f"No PopSets found for {virus_name}.")
 
