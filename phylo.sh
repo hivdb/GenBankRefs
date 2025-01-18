e6fa60fb3cbc1d4c67c5b551f84b15b260577d9f
Kaiming <mads396@gmail.com>
2025-01-18 00:22:45 -0800

    update phylo tree analysis code

--- phylo.sh
+++ phylo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm -f output_L
-ViralMSA.py -s L_isolates.fasta -o output_L -r L_ref_na.fasta
+rm -rf output_L
+ViralMSA.py -e hivdbteam@list.stanford.edu -s L_isolates.fasta -o output_L -r L_ref_na.fasta
 iqtree2 -s L_isolates.fasta.aln -m GTR+G4+F -bb 1000 -nt AUTO
 
-rm -r output_N
-ViralMSA.py -s N_isolates.fasta -o output_N -r N_ref_na.fasta
+rm -rr output_N
+ViralMSA.py -e hivdbteam@list.stanford.edu  -s N_isolates.fasta -o output_N -r N_ref_na.fasta
 iqtree2 -s N_isolates.fasta.aln -m GTR+G4+F -bb 1000 -nt AUTO
