0c72f660e51b13fdf21f44e74c093e0ec7ea14c4
lindazhou <jinruz@stanford.edu>
2025-02-09 23:54:19 -0800

    feat: exclude non-clinical at the start

--- genbank_records.py
+++ genbank_records.py
@@ -33,8 +33,30 @@ def parse_genbank_records(genbank_file):
 
     # total_record = 0
 
+    exclusion_keywords = [
+        'patent',
+        'FDA',
+        'Modified Microbial Nucleic Acid',
+        'CONSTRUCT',
+        'COMPOSITIONS',
+        'monoclonal antibody',
+        'MICROARRAY',
+        'conformation'
+    ]
+
     with open(genbank_file, "r") as handle:
         for record in SeqIO.parse(handle, "genbank"):
+
+            description = record.description.lower()
+
+            # Check if the description contains any exclusion keyword
+            should_exclude = any(
+                keyword.lower() in description for keyword in exclusion_keywords
+            )
+            if should_exclude:
+                excluded_seq_data = filter_by_taxonomy(record, False)
+                excluded_list.append(excluded_seq_data)
+                continue
 
             # total_record += 1
             # if total_record % 10000 == 0:
