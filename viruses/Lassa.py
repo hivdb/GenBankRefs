3445ccbcec53d2af732c2b0eaa8aed3217ee1645
lindazhou <jinruz@stanford.edu>
2025-03-18 15:07:23 -0700

    update: lassa new verision update

--- viruses/Lassa.py
+++ viruses/Lassa.py
@@ -23,7 +23,7 @@ class Lassa(Virus):
 
     @property
     def pubmed_file(self):
-        return self.pubmed_folder / "Reference_Summary_Feb20.xlsx"
+        return self.pubmed_folder / "Reference_Summary_Mar18.xlsx"
 
     @property
     def pubmed_additional_from_gb(self):
